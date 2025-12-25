c00e952c675f3ec5ebf744ca572486fabf1a0fc8
Bidhan012-ops <berabidhan012@gmail.com>
2025-12-25 19:18:40 +0530

    add versal.json and modiify app for the deploy

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const cors=require('cors');
-require('dotenv').config();
-const todoRoutes=require('./routes/todoRoutes');
-const app=express();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const todoRoutes = require("./routes/todoRoutes");
+
+const app = express();
+
+
 app.use(cors());
 app.use(express.json());
 
-app.use(todoRoutes)
 
+app.use("/api", todoRoutes);
 
-
-
-const port=process.env.PORT || 3001;
-const mongoURI=process.env.MONGODB_URI;
-mongoose.connect(mongoURI).then(()=>{
-    console.log("Connected to MongoDB");
-    app.listen(port,()=>{
-        console.log(`Server is running on port http://localhost:${port}`);
-    });
-}).catch((err)=>{
-    console.error("Error connecting to MongoDB:", err);
+app.get("/", (req, res) => {
+  res.send("Todo Backend is running 🚀");
 });
+
+const mongoURI = process.env.MONGODB_URI;
+
+if (!mongoose.connection.readyState) {
+  mongoose
+    .connect(mongoURI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) =>
+      console.error("MongoDB connection error:", err)
+    );
+}
+
+module.exports = app;
